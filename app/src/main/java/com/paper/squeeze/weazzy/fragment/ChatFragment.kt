3783901275d8c49174374922587ae65a66b4c5ff
vineet23 <vineet23@users.noreply.github.com>
2020-05-12 18:13:29 +0530

    Chat fragment done

--- app/src/main/java/com/paper/squeeze/weazzy/fragment/ChatFragment.kt
+++ app/src/main/java/com/paper/squeeze/weazzy/fragment/ChatFragment.kt
@@ -1,10 +1,13 @@
 package com.paper.squeeze.weazzy.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,18 +27,20 @@ class ChatFragment : Fragment() {
         retainInstance = true
     }
 
+    var root:View? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val views: View = inflater.inflate(R.layout.fragment_chat, container, false)
+        if (root == null) {
+            // Inflate the layout for this fragment
+            root = inflater.inflate(R.layout.fragment_chat, container, false)
 
-        //setting up the collapsing toolbar
-        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
-        (activity as AppCompatActivity?)!!.supportActionBar?.setDisplayHomeAsUpEnabled(false)
-
-        return views
+            //setting up the collapsing toolbar
+            (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+            (activity as AppCompatActivity?)!!.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        }
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,6 +51,29 @@ class ChatFragment : Fragment() {
             layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
             adapter = ChatsAdapter()
         }
+
+        //to hide focus
+        searchEditText.clearFocus()
+
+        //to gain focus
+        searchEditText.setOnClickListener {
+            appBarLayout.setExpanded(true)
+            searchEditText.isFocusable = true
+            searchEditText.isFocusableInTouchMode = true
+        }
+
+        //to search and also scroll appbar to expanded state
+        searchEditText.addTextChangedListener( object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                appBarLayout.setExpanded(true)
+            }
+        })
 
     }
 
